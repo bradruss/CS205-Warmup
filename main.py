e3721d4d9ffe7e796dd14b4ee4cfed97d11542c4
Bradley Russavage <brussava@uvm.edu>
2020-09-16 15:59:00 -0400

    Half dict

--- main.py
+++ main.py
@@ -1,32 +1,90 @@
 import sqlite3
 from sqlite3 import Error
+connection = None
 
-def createTables(conn, sqlStatement):
+abbrevDict = {
+"Alabama": "AL", 
+"Alaska": "AK", 
+"Arizona": "AZ", 
+"Arkansas": "AR", 
+"California": "CA", 
+"Colorado": "CO", 
+"Delaware": "DE", 
+"Florida": "FL", 
+"Georgia": "GA", 
+"Hawaii": "HI",
+"Idaho": "ID", 
+"Illinois": "IL", 
+"Indiana": "IN", 
+"Iowa": "IA",
+"Kansas": "KS",
+"Kentucky": "KY",
+"Louisiana": "LA",
+"Maine": "ME",
+"Maryland": "MD",
+"Massachusetts": "MA",
+"Michigan": "MI",
+"Minnesota": "MN",
+"Mississippi": "MS",
+"Missouri": "MO",
+"Montana": "MT",
+"Nebraska": "NE",
+"Nevada": "NV",
+"New Hampshire": "NH",
+"New Jersey": "NJ",
+"New Mexico": "NM",
+"New York": "NY",
+"North Carolina": "NC",
+"North Dakota": "ND",
+"": "",
+"": "",
+"": "",
+"": "",
+"": "",
+"": "",
+"": "",
+"": "",
+"": ""}
+
+
+def createTables():
     try:
-        cursor = conn.cursor()
-        cursor.execute(sqlStatement)
+		global connection
+        cursor = connection.cursor()
+		
+		# Execute statement
+        #cursor.execute(sqlStatement)
 
     except Error as e:
         print(e)
 
 def dbConnect():
-    connection = None
+    global connection
     try:
         connection = sqlite3.connect("nba.db")
+		return connection
 
     except Error as e:
         print(e)
 
-    return connection
 
 def main():
-    conn = dbConnect()
-    if conn is not None:
-        query1 = ""
+    dbConnect()
+	global connection
+    if connection is not None:
+		# should only create tables if not exists
+        createTables()
 
-        createTables(conn, query1)
+		# Perform rest of tasks below
 
-
-    conn.close()
+		
+		
+		
+		
+		
+		
+		
+		
+		connection.close()
 
 main()
